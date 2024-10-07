0e6106b2690c4da0fca62e4b1a8ac5657831d1a1
Simon van Lierde <simonvanlierde@gmail.com>
2024-10-07 21:18:28 +0200

    Some documentation

--- sentier_data_tools/iri.py
+++ sentier_data_tools/iri.py
@@ -8,6 +8,7 @@ VOCAB_FUSEKI = "https://fuseki.d-d-s.ch/skosmos/query"
 
 
 def convert_json_object(obj: dict) -> URIRef | Literal:
+    """Convert a JSON-formatted SPARQL Query Result object into an rdflib URIRef or Literal."""
     if obj["type"] == "literal":
         return Literal(
             obj["value"], lang=obj.get("xml:lang"), datatype=obj.get("datatype")
@@ -21,13 +22,15 @@ def convert_json_object(obj: dict) -> URIRef | Literal:
 
 
 class VocabIRI(URIRef):
+    """Base class for standard queries for IRIs from sentier.dev vocabularies."""
+
     def triples(
         self,
         *,
         iri_position: TriplePosition = TriplePosition.SUBJECT,
         limit: int | None = 25,
     ) -> list[tuple]:
-        """Get triples from the sentier.dev vocabulary for the given IRI.
+        """Get triples from a sentier.dev vocabulary for the given IRI.
 
         Args:
             iri_position (TriplePosition, optional): The position of the IRI in the triple.
@@ -35,11 +38,11 @@ class VocabIRI(URIRef):
             limit (int | None, optional): The maximum number of triples to return. Defaults to 25.
 
         Returns:
-            list[tuple]: A list of triples from the sentier.dev vocabulary.
+            list[tuple]: A list of triples from a sentier.dev vocabulary.
         """
-        # Ensure graph_url is defined in the subclass
+        # Ensure a vocabulary graph_url is defined in the subclass
         if not getattr(self, "graph_url", None):
-            error_msg = f"{self.__class__.__name__}, must define a class-level 'graph_url' attribute."
+            error_msg = f"{self.__class__.__name__}, must define a class-level 'graph_url' attribute to indicate the vocabulary graph URL."
             logger.error(error_msg)
             raise AttributeError(error_msg)
 
@@ -67,7 +70,7 @@ class VocabIRI(URIRef):
         ]
 
     def graph(self, *, iri_position: TriplePosition = TriplePosition.SUBJECT) -> Graph:
-        """Return an `rdflib` graph of the data from the sentier.dev vocabulary for this IRI"""
+        """Return an `rdflib` graph of the data from the sentier.dev vocabulary for this IRI."""
         graph = Graph()
         for triple in self.triples(iri_position=iri_position, limit=None):
             graph.add(triple)
@@ -75,8 +78,12 @@ class VocabIRI(URIRef):
 
 
 class ProductIRI(VocabIRI):
+    """Class for standard queries for IRIs from the products vocabulary."""
+
     graph_url = "https://vocab.sentier.dev/products/"
 
 
 class UnitIRI(VocabIRI):
+    """Class for standard queries for IRIs from the units vocabulary."""
+
     graph_url = "https://vocab.sentier.dev/qudt/"
